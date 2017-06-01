778410084d9664e724791934321a4fb3ef824e35
Robert Kent <git@rooted.ltd.uk>
2017-06-01 16:00:06 +0100

    Avoid multiple logins

--- bin/setup_cf-service-broker.sh
+++ bin/setup_cf-service-broker.sh
@@ -16,13 +16,8 @@ BASE_DIR="`dirname \"$0\"`"
 . "$BASE_DIR/bosh-env.sh"
 
 eval export `prefix_vars "$DEPLOYMENT_FOLDER/bosh-config.sh"`
-eval export `prefix_vars "$DEPLOYMENT_FOLDER/outputs.sh" RABBITMQ_`
-eval export `prefix_vars "$DEPLOYMENT_FOLDER/passwords.sh" RABBITMQ_`
-eval export `prefix_vars "$DEPLOYMENT_FOLDER/cf-credentials-admin.sh" RABBITMQ_`
-
-# Convert from relative to an absolute path
-findpath BOSH_CA_CERT "$BOSH_CA_CERT"
-export BOSH_CA_CERT
+eval export `prefix_vars "$DEPLOYMENT_FOLDER/outputs.sh"`
+eval export `prefix_vars "$DEPLOYMENT_FOLDER/cf-credentials-admin.sh"`
 
 SERVICE_NAME="${1:-$SERVICE_NAME}"
 SERVICE_USERNAME="${2:-$SERVICE_USERNAME}"
@@ -30,6 +25,8 @@ SERVICE_PASSWORD="${3:-$SERVICE_PASSWORD}"
 SERVICE_URL="${4:-$SERVICE_URL}"
 
 [ -z "$SERVICE_URL" ] && FATAL 'Not enough parameters'
+
+[ -n "$DONT_SKIP_SSL_VALIDATION" ] || CF_EXTRA_OPTS='--skip-ssl-validation'
 
 if "$CF" service-brokers | grep -Eq "^$SERVICE_URL$"; then
 	[ -n "$IGNORE_EXISTING" ] && LOG_LEVEL='WARN' || LOG_LEVEL='FATAL'
@@ -39,6 +36,18 @@ if "$CF" service-brokers | grep -Eq "^$SERVICE_URL$"; then
 	exit 0
 fi
 
+if [ -z "$NO_LOGIN" ]; then
+	INFO "Setting API target as $api_dns"
+	"$CF" api "$api_dns" "$CF_EXTRA_OPTS"
+
+	INFO "Logging in as $CF_ADMIN_USERNAME"
+	"$CF" login -u "$CF_ADMIN_USERNAME" -p "$CF_ADMIN_PASSWORD" -s "$SERVICES_SPACE" "$CF_EXTRA_OPTS" 
+else
+	"$CF" target -s "$SERVICES_SPACE"
+fi
+
+INFO "Creating service broker: $SERVICE_NAME"
 "$CF" create-service-broker "$SERVICE_NAME" "$SERVICE_USERNAME" "$SERVICE_PASSWORD" "$SERVICE_URL"
 
+INFO "Enabling service broker: $SERVICE_NAME"
 "$CF" enable-service-access "$SERVICE_NAME"
