08d8fd83868d0f21c9c6958332996f9e46e5249e
Robert Kent <git@rooted.ltd.uk>
2017-07-24 13:05:49 +0100

    Check for STACK_PARAMETERS

--- bin/update_aws_cloudformation.sh
+++ bin/update_aws_cloudformation.sh
@@ -119,7 +119,7 @@ for _action in validate update; do
 		STACK_OUTPUTS="$STACK_OUTPUTS_DIR/outputs-$STACK_NAME.$STACK_OUTPUTS_SUFFIX"
 
 		INFO "Updating: $STACK_PARAMETERS"
-		[ x"$_action" = x"update" ] && update_parameters_file "$CLOUDFORMATION_DIR/$_file" "$STACK_PARAMETERS"
+		[ x"$_action" = x"update" -a -f "$STACK_PARAMETERS" ] && update_parameters_file "$CLOUDFORMATION_DIR/$_file" "$STACK_PARAMETERS"
 
 		aws_change_set "$STACK_NAME" "$STACK_URL" "$STACK_OUTPUTS" "$STACK_PARAMETERS" --template-url $_action || FATAL "Failed to $_action stack: $STACK_NAME, $_file"
 	done
