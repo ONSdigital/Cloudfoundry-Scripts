1192d34e8f3a2f999d9412cc03f8f98416e6f964
Robert Kent <git@rooted.ltd.uk>
2017-08-09 12:45:29 +0100

    Fix IP generation

--- bin/process_cidrs.sh
+++ bin/process_cidrs.sh
@@ -12,6 +12,23 @@ DEFAULT_DEFAULT_ROUTE_OFFEST="${DEFAULT_DEFAULT_ROUTE_OFFEST:-1}"
 DEFAULT_RESERVED_START_OFFSET="${DEFAULT_RESERVED_START_OFFSET:-1}"
 DEFAULT_RESERVED_SIZE="${DEFAULT_RESERVED_SIZE:-10}"
 
+ip_sequence(){
+	local name="$1"
+	local type="$2"
+	local base_ip="$3"
+	local start="$4"
+	local stop="$5"
+
+	local offset
+	local count=0
+
+	for i in `seq "$start" "$stop"`; do
+		count="`expr $count + 1`"
+
+		 echo "${name}_${type}_ip$count='`decimal_to_ip "$base_ip" "$i"`'"
+	done
+}
+
 ip_to_decimal(){
         echo $1 | awk -F. '{sum=$4+($3*256)+($2*256^2)+($1*256^3)}END{printf("%d\n",sum)}'
 }
@@ -42,28 +59,26 @@ eval static_size="\$${NETWORK_UC}_STATIC_SIZE"
 
 DECIMAL_IP="`ip_to_decimal "$CIDR"`"
 
-echo "${NETWORK_NAME}_default_route='`decimal_to_ip "$DECIMAL_IP" "${default_route_offset:-$DEFAULT_DEFAULT_ROUTE_OFFEST}"`'"
-echo "${NETWORK_NAME}_reserved_start='`decimal_to_ip "$DECIMAL_IP" "${reserved_start_offset:-$DEFAULT_RESERVED_START_OFFSET}"`'"
-echo "${NETWORK_NAME}_reserved_stop='`decimal_to_ip "$DECIMAL_IP" "${reserved_size:-$DEFAULT_RESERVED_SIZE}"`'"
+default_route_offset="${default_route_offset:-$DEFAULT_DEFAULT_ROUTE_OFFEST}"
+reserved_start_offset="${reserved_start_offset:-$DEFAULT_RESERVED_START_OFFSET}"
+reserved_size="${reserved_size:-$DEFAULT_RESERVED_SIZE}"
+reserved_stop_offset="`expr "$reserved_start_offset" + "$reserved_size" - 1`"
+
+echo "${NETWORK_NAME}_default_route='`decimal_to_ip "$DECIMAL_IP" "$default_route_offset"`'"
+echo "${NETWORK_NAME}_reserved_start='`decimal_to_ip "$DECIMAL_IP" "$reserved_start_offset"`'"
+echo "${NETWORK_NAME}_reserved_stop='`decimal_to_ip "$DECIMAL_IP" "$reserved_size"`'"
+
+ip_sequence "$NETWORK_NAME" reserved "$DECIMAL_IP" "$reserved_start_offset" "$reserved_stop_offset"
 
 if [ -n "$static_size" ]; then
-	eval reserved_stop="\$${NETWORK_NAME}_reserved_stop"
-
-	[ -z "$static_start_offset" ] && static_start_offset="`expr $reserved_stop + 1`"
+	[ -z "$static_start_offset" ] && static_start_offset="`expr $reserved_stop_offset + 1`"
 
 	static_stop_offset="`expr $static_start_offset + $static_size - 1`"
 
-	[ x"$static_start_offset" = x"$reserved_stop" ] && FATAL "Static start address is the same as the reserved end address: $static_start_offset & $reserved_stop"
+	[ x"$static_start_offset" = x"$reserved_stop_offset" ] && FATAL "Static start address is the same as the reserved end address: $static_start_offset & $reserved_stop_offset"
 
 	echo "${NETWORK_NAME}_static_start='`decimal_to_ip "$DECIMAL_IP" "$static_start_offset"`'"
 	echo "${NETWORK_NAME}_static_stop='`decimal_to_ip "$DECIMAL_IP" "$static_stop_offset"`'"
 
-	for i in `seq $static_start_offset $static_stop_offset`; do
-		eval static_offset="`expr $static_start_offset + ${count:-0}`"
-
-		count="`expr ${count:-0} + 1`"
-
-		echo "${NETWORK_NAME}_static_ip$count='`decimal_to_ip "$DECIMAL_IP" "$static_offset"`'"
-
-	done
+	ip_sequence "$NETWORK_NAME" static "$DECIMAL_IP" "$static_start_offset" "$static_stop_offset"
 fi
