69a321609b1740055694fceae323328498a4d120
Robert Kent <git@rooted.ltd.uk>
2017-08-18 15:19:16 +0100

    Use correct security group

--- bin/setup_cf-rds-broker.sh
+++ bin/setup_cf-rds-broker.sh
@@ -78,7 +78,7 @@ applications:
     AWS_ACCESS_KEY_ID: $rds_broker_access_key_id
     AWS_SECRET_ACCESS_KEY: $rds_broker_secret_access_key
     INSTANCE_TAGS: [ 'name','$deployment_name-Broker-Database' ]
-    AWS_SEC_GROUP: $rds_security_group
+    AWS_SEC_GROUP: $apps_rds_security_group
     AWS_DB_SUBNET_GROUP: $rds_subnet_group
 EOF
 
