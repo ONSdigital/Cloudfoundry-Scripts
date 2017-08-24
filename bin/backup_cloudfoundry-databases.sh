2b52ea0b868a187a4bc03009fa6bd2b41c4131fa
Robert Kent <git@rooted.ltd.uk>
2017-08-24 10:33:47 +0100

    Make Bosh executable

--- bin/backup_cloudfoundry-databases.sh
+++ bin/backup_cloudfoundry-databases.sh
@@ -24,6 +24,8 @@ findpath BOSH_CA_CERT "$BOSH_CA_CERT"
 
 export BOSH_CA_CERT
 
+installed_bin "$BOSH"
+
 INFO "Pointing Bosh at deployed Bosh: $BOSH_ENVIRONMENT"
 "$BOSH" alias-env -e "$BOSH_ENVIRONMENT" "$BOSH_ENVIRONMENT"
 
