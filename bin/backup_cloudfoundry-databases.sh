fa4a5feea52a64396e1c012f6b3bf79c7f0d65d7
Robert Kent <git@rooted.ltd.uk>
2017-09-26 13:47:35 +0100

    Remove comment. Add description

--- bin/backup_cloudfoundry-databases.sh
+++ bin/backup_cloudfoundry-databases.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #
+# Run Bosh errands that are prefxed with 'backup-'
 #
 
 set -e
 
 BASE_DIR="`dirname \"$0\"`"
 
-DEPLOYMENT_NAME="$1"
+DEPLOYMENT_NAME="${1:-$DEPLOYMENT_NAME}"
 
 . "$BASE_DIR/common.sh"
 
@@ -30,7 +31,6 @@ INFO "Pointing Bosh at deployed Bosh: $BOSH_ENVIRONMENT"
 INFO 'Attempting to login'
 "$BOSH" log-in >&2
 
-# Bosh prints everything out to stdout, so we need
 for _e in `"$BOSH" errands | grep -E '^backup-'`; do
 	"$BOSH" run-errand "$_e"
 done
