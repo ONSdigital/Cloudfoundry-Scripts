0a2a1e411bbdce8f2662e49d2e04c174c4f68c4a
Robert Kent <git@rooted.ltd.uk>
2017-10-03 17:28:41 +0100

    Add simple app to view environmental variables

---
+++ basic-apps/env-vars/render-template.py
@@ -0,0 +1,13 @@
+import sys, os, json, re
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return render_template('environ.html',vars=sorted(os.environ.items()), json=json, re=re)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=os.environ.get('PORT','8080'))
+
