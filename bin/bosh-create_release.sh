da8ea52ce164bbb57710070b2e380476fdd9642f
Robert Kent <git@rooted.ltd.uk>
2018-02-05 16:10:02 +0000

    Use provided blob dir and find its full path

--- bin/bosh-create_release.sh
+++ bin/bosh-create_release.sh
@@ -23,11 +23,14 @@ BASE_DIR="`dirname \"$0\"`"
 
 RELEASE_NAME="${1:-$RELEASE_NAME}"
 RELEASE_DIR="${2:-${RELEASE_DIR:-$RELEASE_NAME}}"
+BLOBS_DIR="${3:-$BLOBS_DIR}"
 
-RELEASE_BLOB_SOURCE="${RELEASE_BLOB_SOURCE:-downloads}"
+RELEASE_BLOB_SOURCE="${RELEASE_BLOB_SOURCE:-blobs}"
 RELEASE_BLOB_DESTINATION="${RELEASE_BLOB_DESTINATION:-blobs}"
 
 . "$BASE_DIR/common.sh"
+
+findpath BLOBS_DIR "$BLOBS_DIR"
 
 [ -z "$RELEASE_NAME" ] && FATAL 'No release name provided'
 [ -d "$RELEASE_DIR" ] || FATAL "Bosh release directory does not exist: $RELEASE_NAME"
@@ -44,9 +47,9 @@ version="`cat version.txt`"
 [ -d "config" ] || mkdir config
 [ -f config/blobs.yml ] || touch config/blobs.yml
 
-if [ -d "$RELEASE_BLOB_SOURCE" ]; then
+if [ -d "$BLOBS_DIR" ]; then
 	# We blindy assume/hope that the files don't contain spaces
-	for _s in "$RELEASE_BLOB_SOURCE/"*; do
+	for _s in "$BLOBS_DIR/"*; do
 		[ -f "$_s" ] || continue
 
 		filename="`basename "$_s"`"
