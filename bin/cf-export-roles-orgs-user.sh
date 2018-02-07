88a949fcf9e11f51fd03a8a34140120cc3597d9e
Robert Kent <git@rooted.ltd.uk>
2018-02-07 10:32:21 +0000

    Fix security group export

--- bin/cf-export-roles-orgs-user.sh
+++ bin/cf-export-roles-orgs-user.sh
@@ -124,14 +124,11 @@ cf service-brokers | awk '!/^ *(name.*|Getting .*|OK|No .*|)?$/{ print $1 }' | s
 [ -d asg ] || mkdir -p asg
 echo '. finding security groups'
 for i in `cf security-groups | awk '!/^ *(Name .*|Getting .*|OK|No .*)?$/{ print $2 }'`; do
+for i in `cf security-groups | awk '!/^ *(name .*|Name .*|Getting .*|OK|No .*)?$/{ gsub("^(#[0-9]+)?\\s+","") gsub(" .*$","") g[$0]++ }END{ for(i in g) print i }'`; do
 	echo ".. inspecting security group $i"
-	cf security-groups | awk '!/^ *(name .*|Name .*|Getting .*|OK|No .*)?$/{
-		gsub("^(#[0-9]+)?\\s+","")
-		gsub(" .*$","")
-		g[$0]++
-	}END{
-		for(i in g)
-			print i
+	cf security-group $i | awk '!/^ *(Rules|Name .*|Getting .*|OK|No .*)?$/{
+		gsub("^\t","")
+		print $0
 	}' >"asg/$i.yml"
 
 	echo "cf create-security-group $i asg/$i.yml" >>09_security_groups.sh
