0a1515edb9b167831ffea8977c832090dc696cc0
Robert Kent <git@rooted.ltd.uk>
2018-03-01 18:13:03 +0000

    Update creating release

--- bin/bosh-create_release.sh
+++ bin/bosh-create_release.sh
@@ -24,36 +24,40 @@ BASE_DIR="`dirname \"$0\"`"
 RELEASE_NAME="${1:-$RELEASE_NAME}"
 RELEASE_DIR="${2:-${RELEASE_DIR:-$RELEASE_NAME}}"
 
-RELEASE_BLOB_DESTINATION="${RELEASE_BLOB_DESTINATION:-blobs}"
+RELEASE_BLOB_DESTINATION="$RELEASE_DIR/blobs"
 
 . "$BASE_DIR/common.sh"
+
+set -x
+pwd
 
 # Export INFO() so we can run it under find, _date() is called by INFO()
 export -f INFO _date
 
-[ -d blobs ] && findpath BLOBS_DIR blobs
-
 [ -z "$RELEASE_NAME" ] && FATAL 'No release name provided'
 [ -d "$RELEASE_DIR" ] || FATAL "Bosh release directory does not exist: $RELEASE_NAME"
+[ -d "$RELEASE_BLOB_DESTINATION" ] || mkdir -p "$RELEASE_BLOB_DESTINATION"
 
-shift 1
+# Releases place their blobs under their own folder, so we can automatically add the right blobs
+# to the right releases
+[ -d "blobs/$RELEASE_DIR" ] && findpath BLOBS_DIR "blobs/$RELEASE_DIR"
+
+# Find the fullpath
+findpath RELEASE_BLOB_DESTINATION "$RELEASE_BLOB_DESTINATION"
 
 cd "$RELEASE_DIR"
 
-[ -f "version.txt" ] || echo "0.1.0" >version.txt
+[ -f version.txt ] || echo "0.1.0" >version.txt
 
 version="`cat version.txt`"
 
 # Ensure required dirs & files exist
-[ -d "config" ] || mkdir config
+[ -d config ] || mkdir config
 [ -f config/blobs.yml ] || touch config/blobs.yml
 
-if [ -n "$BLOBS_DIR" -a -d "$BLOBS_DIR" ]; then
-	cd "$BLOBS_DIR"
-
-	INFO 'Adding blobs'
-	find . -type f -exec sh -xc "pwd;INFO '. adding {}'; '$BOSH_CLI' add-blob --dir "$RELEASE_DIR" --tty '{}' '$RELEASE_BLOB_DESTINATION/{}'" \;
+if [ -n "$BLOBS_DIR" ]; then
+	find "$BLOBS_DIR" -type f -exec sh -xc "INFO '. adding {}'; '$BOSH_CLI' add-blob --tty '{}' '$RELEASE_BLOB_DESTINATION'" \;
 fi
 
 INFO "Creating release: $RELEASE_NAME"
-"$BOSH_CLI" create-release --tty --force --dir "$RELEASE_DIR" --version="$version" --tarball "$RELEASE_NAME.tgz"
+"$BOSH_CLI" create-release --tty --force --version="$version" --tarball "$RELEASE_NAME.tgz"
