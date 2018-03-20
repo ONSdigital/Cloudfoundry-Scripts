beeeec6bb1e792abddbeb332ac994389007e88aa
Sam Bryant <srbry@hotmail.com>
2018-03-20 10:04:53 +0000

    renamed lite to director and full to cf in scripts to keep inline with manifests

--- bin/delete_cloudfoundry.sh
+++ bin/delete_cloudfoundry.sh
@@ -17,7 +17,7 @@ BASE_DIR="`dirname \"$0\"`"
 . "$BASE_DIR/common-bosh.sh"
 . "$BASE_DIR/common-bosh-login.sh"
 
-# Needed for the Bosh Lite manifest
+# Needed for the Bosh Director manifest
 INFO 'Loading Bosh config'
 export_file_vars "$BOSH_DIRECTOR_CONFIG"
 
@@ -25,12 +25,12 @@ INFO 'Deleting Bosh Deployment'
 "$BOSH_CLI" delete-deployment --force --tty
 
 INFO 'Deleting Bosh bootstrap environment'
-"$BOSH_CLI" delete-env --tty --state="$BOSH_LITE_STATE_FILE" "$BOSH_LITE_INTERPOLATED_MANIFEST" || FATAL 'Bosh environment deletion failed'
+"$BOSH_CLI" delete-env --tty --state="$BOSH_DIRECTOR_STATE_FILE" "$BOSH_DIRECTOR_INTERPOLATED_MANIFEST" || FATAL 'Bosh environment deletion failed'
 
 # It 'should' exist
-if [ -f "$BOSH_LITE_STATE_FILE" ]; then
-	INFO "Removing Bosh state file: $BOSH_LITE_STATE_FILE"
-	rm -f "$BOSH_LITE_STATE_FILE"
+if [ -f "$BOSH_DIRECTOR_STATE_FILE" ]; then
+	INFO "Removing Bosh state file: $BOSH_DIRECTOR_STATE_FILE"
+	rm -f "$BOSH_DIRECTOR_STATE_FILE"
 fi
 
 INFO 'Successfully deleted Bosh environment'
